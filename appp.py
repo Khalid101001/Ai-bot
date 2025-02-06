49d11811c63df9e62ab1ddf2581a152c489fbd95
Khalid101001 <c.ycyvyc932@gmail.com>
2025-02-06 11:28:46 +0300

    Update and rename app.py to appp.py

--- appp.py
+++ appp.py
@@ -4,9 +4,9 @@ import time
 
 api_id = '25875948'  # ضع API_ID الخاص بك
 api_hash = 'bbc8cd4753b320c932bd56254d2917a0'  # ضع API_HASH الخاص بك
+bot_token = "7111720840:AAFcf5qbJ7h-yG0OFHIXUexu4eoU57r4l-A"  # تمت إضافة التوكن هنا
 
-client = TelegramClient('ai_bot', api_id, api_hash)
-
+client = TelegramClient('ai_bot', api_id, api_hash).start(bot_token=bot_token)
 
 AI_API = "https://atared.serv00.net/s/rq.php?text="
 
@@ -14,24 +14,20 @@ AI_API = "https://atared.serv00.net/s/rq.php?text="
 async def handle_message(event):
     try:
         if event.is_private:
-            
             user_message = event.message.text
-            
-        
             response = requests.get(f"{AI_API}{user_message}")
             
             if response.status_code == 200:
                 ai_reply = response.text
                 await event.reply(ai_reply)
-                print(f":الرد {event.sender_id} ")
+                print(f":الرد {event.sender_id}")
             else:
-                await event.reply("خطأ بال api يمكن")
+                await event.reply("خطأ بال API يمكن")
             
-            time.sleep(2)  
+            time.sleep(2)
 
     except Exception as e:
         print(f"خطأ: {str(e)}")
 
 print("اشتغل...")
-client.start()
 client.run_until_disconnected()
